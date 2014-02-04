8638907c91296c0981dbe5feeb9aaab186d83aba
Kyle Marek-Spartz <zeckalpha@gmail.com>
2014-02-04 13:15:38 -0600

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='interface-mixins',
-    version='0.0.1',
+    version='0.0.2',
     description='Interfaces for Python',
     long_description=open('README.rst').read(),
     author='Kyle Marek-Spartz',
